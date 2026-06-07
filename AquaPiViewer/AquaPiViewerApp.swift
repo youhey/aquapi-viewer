f3e56038e28551f66fb0336cf5439cc5d698667c
IKEDA Youhei <youhey.ikeda@gmail.com>
2026-06-07 15:36:09 +0900

    New Xcode Project

---
+++ AquaPiViewer/AquaPiViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaPiViewerApp.swift
+//  AquaPiViewer
+//
+//  Created by 池田洋平 on 2026/06/07.
+//
+
+import SwiftUI
+
+@main
+struct AquaPiViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AquaPiViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AquaPiViewer
+//
+//  Created by 池田洋平 on 2026/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
